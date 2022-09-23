c86a2db698cb78efc5448d222030266a85d76c31
emilyzimmerman <zimmermanemily121@gmail.com>
2022-09-22 19:43:45 -0500

    Exercise 3 update
    I have complete exercise 3 with the string interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ChildOneComponent } from './child-one/child-one.component';
-import { ChildTwoComponent } from './child-two/child-two.component';
-import { ChildThreeComponent } from './child-three/child-three.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ChildOneComponent,
-    ChildTwoComponent,
-    ChildThreeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
