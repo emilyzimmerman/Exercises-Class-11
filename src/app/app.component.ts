a01e2fae23d9d931c12bce0192a90011bff17c8c
emilyzimmerman <zimmermanemily121@gmail.com>
2022-09-26 19:43:22 -0500

    Exercise 2 fixed
    Fixed exercise 2's bugs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-    clickLog = console.log('Hello World!')
+    clickLog() {
+      console.log('Hello World!')
+    };
   }
 
